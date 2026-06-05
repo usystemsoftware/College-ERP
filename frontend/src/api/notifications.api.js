b22c7ae5be56a2e01206b74c5c8725f097977a79
nivedita-utech <nivedita@usoftware.cloud>
2026-06-05 10:21:51 +0530

    changes

---
+++ frontend/src/api/notifications.api.js
@@ -0,0 +1,17 @@
+import client from './client';
+
+export const getMyNotifications = async (params) => {
+  return await client.get('/notifications/my', { params });
+};
+
+export const markAsRead = async (id) => {
+  return await client.patch(`/notifications/${id}/read`);
+};
+
+export const markAllAsRead = async () => {
+  return await client.patch('/notifications/mark-all-read');
+};
+
+export const deleteNotification = async (id) => {
+  return await client.delete(`/notifications/${id}`);
+};
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 import store from './app/store';
 import AppRoutes from './routes/AppRoutes';
 import './index.css';
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
+        <Toaster position="top-right" />
         <AppRoutes />
       </BrowserRouter>
     </Provider>

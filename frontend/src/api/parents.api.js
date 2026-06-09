2f7bf245c56d13fddda2dc59afb7eb9f1146e351
shubhamusystem <shubham@usoftware.cloud>
2026-06-09 18:01:56 +0530

    add the parent section

--- frontend/src/api/parents.api.js
+++ frontend/src/api/parents.api.js
@@ -4,6 +4,5 @@ export const getParentsAPI = (params) => api.get('/parents', { params });
 export const getParentByIdAPI = (id) => api.get(`/parents/${id}`);
 export const getMyParentProfileAPI = () => api.get('/parents/me');
 export const createParentAPI = (data) => api.post('/parents', data);
-export const getMyParentProfileAPI = () => api.get('/parents/me');
 export const updateParentAPI = (id, data) => api.put(`/parents/${id}`, data);
 export const deleteParentAPI = (id) => api.delete(`/parents/${id}`);
